9f1d8d16dd89e2d8c4733336e401a79f92d15297
WireTap1337 <deletustheschool@outlook.com>
2023-04-22 10:18:14 +0200

    Update every 5s ja mislim jer ono sad radi ka pa

--- PrometSplit.js
+++ PrometSplit.js
@@ -7,36 +7,51 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(mymap);
 
-// Pingaj API
-fetch('https://api.split.prometko.si/vehicles')
-  .then(response => response.json())
-  .then(data => {
-    // Define a function to create the custom marker
-    function createBusMarker(bus) {
-      // Get the bus line name from the bus object
-      const busLine = bus.routeShortName;
+// Define a function to create the custom marker
+function createBusMarker(bus) {
+  // Get the bus line name from the bus object
+  const busLine = bus.routeShortName;
 
-      // Create a new marker with the custom icon
-      const marker = L.marker(
-        [bus.latitude, bus.longitude], 
-        {
-          icon: L.divIcon({
-            className: 'bus-icon',
-            html: `<div class="bus-circle">${busLine}</div>`,
-            iconSize: [32, 32]
-          })
-        }
-      );
-
-      return marker;
+  // Create a new marker with the custom icon
+  const marker = L.marker(
+    [bus.latitude, bus.longitude], 
+    {
+      icon: L.divIcon({
+        className: 'bus-icon',
+        html: `<div class="bus-circle">${busLine}</div>`,
+        iconSize: [32, 32]
+      })
     }
+  );
 
-    // loop through the bus data and add markers to the map
-    data.data.forEach(bus => {
-      if (bus.latitude && bus.longitude) {
-        const marker = createBusMarker(bus);
-        marker.addTo(mymap);
-      }
-    });
-  })
-  .catch(error => console.error(error));
+  return marker;
+}
+
+// Define a function to update the bus markers on the map
+function updateBusMarkers() {
+  console.log('Updating bus markers...');
+  // Fetch the bus data from the API
+  fetch('https://api.split.prometko.si/vehicles')
+    .then(response => response.json())
+    .then(data => {
+      console.log('Fetched bus data:', data);
+      // Remove existing bus markers from the map
+      mymap.eachLayer(layer => {
+        if (layer.options.icon && layer.options.icon.options.className === 'bus-icon') {
+          mymap.removeLayer(layer);
+        }
+      });
+
+      // loop through the bus data and add new markers to the map
+      data.data.forEach(bus => {
+        if (bus.latitude && bus.longitude) {
+          const marker = createBusMarker(bus);
+          marker.addTo(mymap);
+        }
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+// Call the updateBusMarkers function every 5 seconds
+setInterval(updateBusMarkers, 5000);
