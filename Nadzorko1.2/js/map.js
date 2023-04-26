bc3dee04c4a41785796b6f94e9e5a9bd1a20a83c
WireTap1337 <deletustheschool@outlook.com>
2023-04-26 11:10:51 +0200

    fix map (still keeping mapbox stuff till i implement yes)

--- Nadzorko1.2/js/map.js
+++ Nadzorko1.2/js/map.js
@@ -3,6 +3,20 @@ var mymap = L.map('mapid', {
 }).setView([43.5081, 16.4402], 13);
 var accessToken = 'pk.eyJ1IjoidmVrZWpzbiIsImEiOiJja25ubHFoMWIxNXZ0MnNwbjFtaW1nbnFmIn0.8pX2xs2EKOq75QjXqkxAFQ';
 
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 23,
+    crossOrigin: 'anonymous',
+    id: 'osm'
+}).addTo(mymap);
+
+var darkMatterLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxZoom: 23,
+    crossOrigin: 'anonymous',
+    id: 'darkMatter'
+});
+
 var darkLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
     attribution: '',
     maxZoom: 23,
@@ -12,7 +26,7 @@ var darkLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/ti
     id: 'dark',
     // Set the default layer to be the dark layer
     // You can change this to any of the other layers if you want a different default
-}).addTo(mymap);
+});
 
 var lightLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
     attribution: '',
@@ -48,10 +62,12 @@ var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satell
     zoomOffset: -1,
     crossOrigin: 'anonymous',
     id: 'satellite',
-});
+}); 
 
 // Add the layer control button to the map
 var baseMaps = {
+    "OSM": osmLayer,
+    "DarkMatter": darkMatterLayer,
     "Dark": darkLayer,
     "Light": lightLayer,
     "Outdoors": outdoorsLayer,
