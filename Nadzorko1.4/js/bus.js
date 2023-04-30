03c98e6d97a02c191b85659558659c791963c311
lumisphere <deletusdaschool@gmail.com>
2023-04-30 21:53:55 +0200

    okay im just adding this as a base for tommorw cuh like if i break something so i can come back + a lot of shit already is changed from 1.2 to 1.3, so

---
+++ Nadzorko1.4/js/bus.js
@@ -0,0 +1,86 @@
+let inactiveMarkers = [];
+
+var baseapi = 'https://api.split.prometko.si'
+
+// Function to fetch bus data from the API
+async function fetchBusData() {
+    const response = await fetch(baseapi + '/vehicles');
+    const data = await response.json();
+    return data.success ? data.data : [];
+  }
+  
+  // Function to create bus marker
+  function createBusMarker(bus, isActive) {
+    const markerElement = document.createElement("div");
+    markerElement.className = `bus-marker ${isActive ? "active" : "inactive"}`;
+  
+    const routeShortName = document.createElement("span");
+    routeShortName.className = "route-short-name";
+    routeShortName.textContent = bus.routeShortName;
+    markerElement.appendChild(routeShortName);
+  
+    const busCircle = document.createElement("div");
+    busCircle.className = "bus-circle";
+    markerElement.appendChild(busCircle);
+  
+    const garageNumber = document.createElement("div");
+    garageNumber.className = "garage-number";
+    garageNumber.textContent = bus.garageNumber;
+    markerElement.appendChild(garageNumber);
+  
+    const pointer = document.createElement("img");
+    pointer.className = "pointer";
+    pointer.src = "ico/bussmjer.svg";
+    markerElement.appendChild(pointer);
+  
+    return markerElement;
+  }
+  
+// Function to update bus markers
+async function updateBusMarkers(map, markers) {
+  const busData = await fetchBusData();
+
+  busData.forEach((bus) => {
+    const timeDifference = new Date() - new Date(bus.timestamp);
+    const opacity = timeDifference > 5 * 60 * 1000 ? 0.1 : 1;
+
+    let marker = markers.find((m) => m.options.id === bus.id);
+    const isActive = opacity === 1;
+    const busMarkerElement = createBusMarker(bus, isActive);
+
+    if (!marker) {
+      // busMarkerElement.style.opacity = opacity;
+
+      marker = L.marker([bus.latitude, bus.longitude], {
+        icon: L.divIcon({
+          className: "",
+          html: busMarkerElement.outerHTML,
+          iconAnchor: [12.5, 12.5],
+        }),
+        id: bus.id,
+      }).addTo(map);
+      markers.push(marker);
+
+      if (!isActive) {
+        inactiveMarkers.push(marker);
+      }
+    } else {
+      marker.slideTo([bus.latitude, bus.longitude], {
+        duration: 500, // Animation duration in milliseconds
+        keepAtCenter: false,
+      });
+      marker.getElement().style.opacity = opacity;
+    }
+
+    // Rotate the pointer based on the bus bearing
+    const pointer = marker.getElement().querySelector(".pointer");
+    pointer.style.transform = `rotate(${bus.bearing + 225}deg)`;
+  });
+}
+  
+  const markers = [];
+  // Call the updateBusMarkers function to update the markers on the map
+  updateBusMarkers(mymap, markers);
+  
+  // Update the bus markers every 1 second
+  setInterval(() => updateBusMarkers(mymap, markers), 1000);
---
+++ Nadzorko1.4/js/map.js
@@ -0,0 +1,115 @@
+var mymap = L.map('mapid', {
+    zoomControl: false,
+    attributionControl: false // Disable the default attribution control
+}).setView([43.5081, 16.4402], 13);
+
+var customAttributionControl = L.control.attribution({
+    prefix: false, // Remove the "Leaflet" text
+}).addTo(mymap);
+
+
+var accessToken = 'pk.eyJ1IjoiYW5hbHdvb2QiLCJhIjoiY2xneWJlMm9lMDd2MzNtcDQwOXJwcGtwbCJ9.WwrjO_cMftXcXMOZgucD9Q';
+var maptilerAPI = 'v0X1HSm5SNBEcBxVk7iB';
+
+var darkMatterLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '',
+    maxZoom: 23,
+    crossOrigin: 'anonymous',
+    id: 'darkMatter'
+}).addTo(mymap);
+
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '',
+    maxZoom: 19,
+    crossOrigin: 'anonymous',
+    id: 'osm'
+});
+
+var maptilerdarkLayer = L.tileLayer('https://api.maptiler.com/maps/streets-v2-dark/{z}/{x}/{y}.png?key=' + maptilerAPI, {
+  attribution: '',
+  maxZoom: 22,
+});
+
+var darkLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: '',
+    maxZoom: 23,
+    tileSize: 512,
+    zoomOffset: -1,
+    crossOrigin: 'anonymous',
+    id: 'dark',
+
+});
+
+var lightLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: '',
+    maxZoom: 23,
+    tileSize: 512,
+    zoomOffset: -1,
+    crossOrigin: 'anonymous',
+    id: 'light',
+});
+
+var outdoorsLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: '',
+    maxZoom: 23,
+    tileSize: 512,
+    zoomOffset: -1,
+    crossOrigin: 'anonymous',
+    id: 'outdoors',
+});
+
+var streetsLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: '',
+    maxZoom: 23,
+    tileSize: 512,
+    zoomOffset: -1,
+    crossOrigin: 'anonymous',
+    id: 'streets',
+});
+
+var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+    attribution: '',
+    maxZoom: 23,
+    tileSize: 512,
+    zoomOffset: -1,
+    crossOrigin: 'anonymous',
+    id: 'satellite',
+}); 
+
+// Set the initial selected layer
+document.querySelector('.layer-option[data-layer="darkmatter"]').classList.add('selected');
+
+// Function to change the active tile layer
+function changeTileLayer(layerId) {
+  const layers = {
+    darkmatter: darkMatterLayer,
+    osm: osmLayer,
+    dark: darkLayer,
+    light: lightLayer,
+    outdoors: outdoorsLayer,
+    streets: streetsLayer,
+    satellite: satelliteLayer,
+    maptilerdark: maptilerdarkLayer,
+  };
+
+  mymap.removeLayer(layers[mymap.activeLayer]);
+  mymap.addLayer(layers[layerId]);
+  mymap.activeLayer = layerId;
+}
+
+// Add event listeners to layer options
+const layerOptions = document.querySelectorAll('.layer-option');
+layerOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    changeTileLayer(option.dataset.layer);
+
+    // Update the selected layer style
+    layerOptions.forEach((opt) => {
+      opt.classList.remove('selected');
+    });
+    option.classList.add('selected');
+  });
+});
+
+// Set the initial active layer
+mymap.activeLayer = 'darkmatter';
