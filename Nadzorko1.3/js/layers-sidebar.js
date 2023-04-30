da32f359da6731cefd6426feba04f31708e3f365
lumisphere <deletusdaschool@gmail.com>
2023-04-30 21:34:39 +0200

    big changes, proper implementation on the layers

--- Nadzorko1.3/js/layers-sidebar.js
+++ Nadzorko1.3/js/layers-sidebar.js
@@ -4,3 +4,25 @@ const layersSidebar = document.getElementById('layers-sidebar');
 layersToggle.addEventListener('click', () => {
   layersSidebar.classList.toggle('open');
 });
+
+document.querySelectorAll('.provider-title').forEach((title) => {
+  title.addEventListener('click', () => {
+    const layers = title.nextElementSibling;
+    const arrowIcon = title.querySelector('i');
+
+    if (layers.style.maxHeight === '0px' || layers.style.maxHeight === '') {
+      layers.style.maxHeight = layers.scrollHeight + 'px';
+      arrowIcon.style.transform = 'rotate(180deg)';
+    } else {
+      layers.style.maxHeight = '0px';
+      arrowIcon.style.transform = 'rotate(0deg)';
+    }
+  });
+});
+
+document.querySelectorAll('.layer-option').forEach((option) => {
+  option.addEventListener('click', () => {
+    const layerName = option.textContent;
+    // Implement the logic for changing the tile layer
+  });
+});
--- Nadzorko1.3/js/map.js
+++ Nadzorko1.3/js/map.js
@@ -1,25 +1,32 @@
 var mymap = L.map('mapid', {
-    zoomControl: false
+    zoomControl: false,
+    attributionControl: false // Disable the default attribution control
 }).setView([43.5081, 16.4402], 13);
+
+var customAttributionControl = L.control.attribution({
+    prefix: false, // Remove the "Leaflet" text
+}).addTo(mymap);
+
+
 var accessToken = 'pk.eyJ1IjoiYW5hbHdvb2QiLCJhIjoiY2xneWJlMm9lMDd2MzNtcDQwOXJwcGtwbCJ9.WwrjO_cMftXcXMOZgucD9Q';
 var maptilerAPI = 'v0X1HSm5SNBEcBxVk7iB';
 
-var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19,
-    crossOrigin: 'anonymous',
-    id: 'osm'
-});
-
 var darkMatterLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: '',
     maxZoom: 23,
     crossOrigin: 'anonymous',
     id: 'darkMatter'
 }).addTo(mymap);
 
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '',
+    maxZoom: 19,
+    crossOrigin: 'anonymous',
+    id: 'osm'
+});
+
 var maptilerdarkLayer = L.tileLayer('https://api.maptiler.com/maps/streets-v2-dark/{z}/{x}/{y}.png?key=' + maptilerAPI, {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://maptiler.com/">MapTiler</a>',
+  attribution: '',
   maxZoom: 22,
 });
 
@@ -33,7 +40,7 @@ var darkLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/ti
 
 });
 
-var lightLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+var lightLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
     attribution: '',
     maxZoom: 23,
     tileSize: 512,
@@ -68,3 +75,58 @@ var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satell
     crossOrigin: 'anonymous',
     id: 'satellite',
 }); 
+
+// Set the initial selected layer
+document.querySelector('.layer-option[data-layer="darkmatter"]').classList.add('selected');
+
+// Function to change the active tile layer
+function changeTileLayer(layerId) {
+  const layers = {
+    darkmatter: darkMatterLayer,
+    osm: osmLayer,
+    dark: darkLayer,
+    light: lightLayer,
+    outdoors: outdoorsLayer,
+    streets: streetsLayer,
+    satellite: satelliteLayer,
+    maptilerdark: maptilerdarkLayer,
+  };
+
+  mymap.removeLayer(layers[mymap.activeLayer]);
+  mymap.addLayer(layers[layerId]);
+  mymap.activeLayer = layerId;
+}
+
+// Add event listeners to layer options
+const layerOptions = document.querySelectorAll('.layer-option');
+layerOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    changeTileLayer(option.dataset.layer);
+
+    // Update the selected layer style
+    layerOptions.forEach((opt) => {
+      opt.classList.remove('selected');
+    });
+    option.classList.add('selected');
+  });
+});
+
+// Set the initial active layer
+mymap.activeLayer = 'darkmatter';
+
+mymap.attributionControl.setPrefix(false);
+mymap.attributionControl.addAttribution('');
+
+// Function to toggle attributions
+function toggleAttributions() {
+  const checkbox = document.getElementById('toggle-attributions');
+  
+  if (checkbox.checked) {
+    mymap.attributionControl.setPrefix('&copy; <a href="https://leafletjs.com">Leaflet</a>');
+  } else {
+    mymap.attributionControl.setPrefix(false);
+  }
+}
+
+// Add event listener to the "Show attributions" toggle
+document.getElementById('toggle-attributions').addEventListener('change', toggleAttributions);
