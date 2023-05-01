51f1a06d664bd6c94424f033e3ba68ced6e2a71f
lumisphere <deletusdaschool@gmail.com>
2023-05-01 18:49:20 +0200

    Doesnt fully work but ill fix later

--- Nadzorko1.4/js/businfo.js
+++ Nadzorko1.4/js/businfo.js
@@ -8,37 +8,55 @@ function toggleBusInfo(visible) {
       busInfo.classList.add("hidden");
     }
   }
-
-  document.getElementById("close-bus-info").addEventListener("click", () => {
-    toggleBusInfo(false);
-  });
-
+  
   async function fetchBusDetails(garageNumber) {
-    const response = await fetch("json/buses.json"); // update the path to the JSON file
+    const response = await fetch("json/buses.json");
     const data = await response.json();
-    console.log("Fetched JSON data:", data);
-    console.log("Searching for garage number:", garageNumber);
     return data.find((bus) => bus.id === parseInt(garageNumber, 10).toString());
   }
-
+  
   async function updateBusInfo(garageNumber) {
     const busDetails = await fetchBusDetails(garageNumber);
-    console.log(busDetails); // Add this line to log the bus details
-
+  
     if (busDetails) {
-        const busInfo = document.getElementById("bus-info");
-        busInfo.innerHTML = `
-            <button id="close-bus-info"><i class="fa-solid fa-xmark" style="color: #ffffff;"></i></button>
-            <img src="${busDetails.imageUrl}" alt="Bus Image" width="330" height="210" style="border-radius: 25px;" />
-            <div class="bus-info-text">
-                <p><strong>Model:</strong> ${busDetails.model}</p>
-                <p><strong>Type:</strong> ${busDetails.type}</p>
-                <p><strong>Plates:</strong> ${busDetails.plates}</p>
-            </div>
+      const busResponse = await fetch("https://api.split.prometko.si/vehicles");
+      const busData = await busResponse.json();
+      const busInfo = busData.data.find((bus) => bus.garageNumber === garageNumber);
+  
+      if (busInfo) {
+        const busId = busInfo.id;
+        const vehicleResponse = await fetch(`https://api.split.prometko.si/vehicle/${busId}`);
+        const vehicleData = await vehicleResponse.json();
+  
+        const routeName = vehicleData.data.routeName;
+        const pathwayName = vehicleData.data.fulfilmentRecord.pathwayName;
+        const currentSpeed = Math.round(vehicleData.data.currentSpeed);
+        const timestamp = new Date(vehicleData.data.timestamp).toLocaleString();
+        const delayStartTime = new Date(vehicleData.data.fulfilmentRecord.delayStartTime).toLocaleString();
+  
+        const busInfoElement = document.getElementById("bus-info");
+        busInfoElement.innerHTML = `
+          <button id="close-bus-info"><i class="fa-solid fa-xmark" style="color: #ffffff;"></i></button>
+          <img src="${busDetails.imageUrl}" alt="Bus Image" width="330" height="210" style="border-radius: 25px;" />
+          <div class="bus-info-text">
+            <p><strong>Model:</strong> ${busDetails.model}</p>
+            <p><strong>Type:</strong> ${busDetails.type}</p>
+            <p><strong>Plates:</strong> ${busDetails.plates}</p>
+          </div>
+          <div class="bus-info-right">
+            <p><strong>${routeName}</strong> - ${pathwayName}</p>
+            <p><strong>Current Speed:</strong> ${currentSpeed} km/h</p>
+            <p><strong>Timestamp:</strong> ${timestamp}</p>
+            <p><strong>Delay Start Time:</strong> ${delayStartTime}</p>
+          </div>
+          <div class="bus-info-bottom">
+            <p><strong>Garage Number:</strong> ${garageNumber}</p>
+          </div>
         `;
-
+  
         document.getElementById("close-bus-info").addEventListener("click", () => {
-            toggleBusInfo(false);
+          toggleBusInfo(false);
         });
+      }
     }
-}
+  }
