b1f2fdd0886f91ca968b282f296964dfc3a2dc43
lumisphere <deletusdaschool@gmail.com>
2023-05-03 22:09:59 +0200

    this gonna be a whole ass recode, only base map added for now

---
+++ Nadzorko2.0/js/map.js
@@ -0,0 +1,6 @@
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://demotiles.maplibre.org/style.json', // Use this style URL
+    center: [16.4378, 43.5081], // Split, Croatia
+    zoom: 12,
+  });
