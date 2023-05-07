e86655630718840c9cc55b69318a3539a7bcdfe1
lumisphere <deletusdaschool@gmail.com>
2023-05-07 17:52:24 +0200

    added somewhat working bus markers.

--- js/bus.js
+++ js/bus.js
@@ -1,88 +1,84 @@
-let inactiveMarkers = [];
-
-const baseapi = 'https://api.split.prometko.si';
+// Define the base API URL
+const baseApi = 'https://api.split.prometko.si';
 
 // Function to fetch bus data from the API
 async function fetchBusData() {
-  const response = await fetch(`${baseapi}/vehicles`);
+  const response = await fetch(`${baseApi}/vehicles`);
   const data = await response.json();
   return data.success ? data.data : [];
 }
 
-// Function to update the slide duration globally
-function updateSlideDuration(newSlideDuration) {
-  slideDuration = newSlideDuration;
+// Function to create bus marker
+function createBusMarker(bus, isActive) {
+  const markerElement = document.createElement("div");
+  markerElement.className = `bus-marker ${isActive ? "active" : "inactive"}`;
+
+  const routeShortName = document.createElement("span");
+  routeShortName.className = "route-short-name";
+  routeShortName.textContent = bus.routeShortName;
+  markerElement.appendChild(routeShortName);
+
+  const busCircle = document.createElement("div");
+  busCircle.className = "bus-circle";
+  markerElement.appendChild(busCircle);
+
+  const garageNumber = document.createElement("div");
+  garageNumber.className = "garage-number";
+  garageNumber.textContent = bus.garageNumber;
+  markerElement.appendChild(garageNumber);
+
+  const pointer = document.createElement("img");
+  pointer.className = "pointer";
+  pointer.src = "ico/bussmjer.svg";
+  markerElement.appendChild(pointer);
+
+  return markerElement;
 }
 
 // Function to update bus markers
-async function updateBusMarkers(map) {
-    const busData = await fetchBusData();
-  
-    // Create a GeoJSON FeatureCollection for the bus markers
-    const busMarkersFeatures = {
-      type: 'FeatureCollection',
-      features: busData.map((bus) => {
-        const timeDifference = new Date() - new Date(bus.timestamp);
-        const opacity = timeDifference > 5 * 60 * 1000 ? 0.1 : 1;
-        const isActive = opacity === 1;
-  
-        return {
-            type: 'Feature',
-            properties: {
-              id: `bus-${bus.id}`,
-              routeShortName: bus.routeShortName,
-              garageNumber: bus.garageNumber,
-              bearing: bus.bearing,
-              opacity: opacity,
-              size: 25, // Set size to 25px
-            },
-            geometry: {
-              type: 'Point',
-              coordinates: [bus.longitude, bus.latitude],
-            },
-          };
-      }),
-    };
-  
-    // If the bus markers source does not exist, create it and add the layers
-    if (!map.getSource('bus-markers')) {
-      // Add the bus markers source
-      map.addSource('bus-markers', {
-        type: 'geojson',
-        data: busMarkersFeatures,
-      });
-  
-      // Add the bus markers circle layer
-      map.addLayer({
-        id: 'bus-markers',
-        type: 'circle',
-        source: 'bus-markers',
-        paint: {
-          'circle-radius': ['get', 'size'],
-          'circle-color': '#FFC107',
-          'circle-opacity': ['get', 'opacity'],
-        },
-      });
-  
-      // Add the route short name layer
-      map.addLayer({
-        id: 'bus-markers',
-        type: 'circle',
-        source: 'bus-markers',
-        paint: {
-          'circle-radius': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            12, ['get', 'size'],
-            22, ['*', ['get', 'size'], Math.pow(2, 22 - 12)],
-          ],
-          'circle-color': '#FFC107',
-          'circle-opacity': ['get', 'opacity'],
-        },
-      });
+async function updateBusMarkers(map, markers) {
+  const busData = await fetchBusData();
+
+  busData.forEach((bus) => {
+    const timeDifference = new Date() - new Date(bus.timestamp);
+    const opacity = timeDifference > 5 * 60 * 1000 ? 0.1 : 1;
+
+    let marker = markers.find((m) => m.id === bus.id);
+    const isActive = opacity === 1;
+    const busMarkerElement = createBusMarker(bus, isActive);
+
+    if (!marker) {
+      marker = new maplibregl.Marker({
+        element: busMarkerElement,
+        anchor: 'center'
+      }).setLngLat([bus.longitude, bus.latitude]).addTo(map);
+      marker.id = bus.id;
+      markers.push(marker);
+
+      if (!isActive) {
+        marker.getElement().style.opacity = opacity;
+      }
     } else {
-      // If the bus markers source exists, update its data
-      map.getSource('bus-markers').setData(busMarkersFeatures);
+        marker.setLngLat([bus.longitude, bus.latitude]);
+
+      if (marker.getElement().classList.contains('inactive') && isActive) {
+        const index = markers.indexOf(marker);
+        markers.splice(index, 1);
+        marker.remove();
+      } else {
+        marker.getElement().style.opacity = opacity;
+      }
     }
-  }  
+
+    // Rotate the pointer based on the bus bearing
+    const pointer = marker.getElement().querySelector(".pointer");
+    pointer.style.transform = `rotate(${bus.bearing + 225}deg)`;
+  });
+}
+
+// Call the updateBusMarkers function to update the markers on the map
+const markers = [];
+updateBusMarkers(map, markers);
+
+// Update the bus markers every 1 second
+setInterval(() => updateBusMarkers(map, markers), 1000);
--- js/sidebar.js
+++ js/sidebar.js
@@ -5,3 +5,14 @@ sidebarToggle.addEventListener("click", () => {
   sidebarToggle.classList.toggle("open");
   sidebar.classList.toggle("open");
 });
+
+const toggleBuses = document.getElementById("toggle-buses");
+const inactiveBuses = document.querySelectorAll(".bus.inactive");
+
+toggleBuses.addEventListener("change", () => {
+  if (toggleBuses.checked) {
+    inactiveBuses.forEach(bus => bus.style.display = "block");
+  } else {
+    inactiveBuses.forEach(bus => bus.style.display = "none");
+  }
+});
